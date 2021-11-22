64f399e3c1d9723a9649a80522db71ab85b2c294
Mario Font <mariofont@icloud.com>
2021-11-22 12:12:49 +0100

    Added Splitbee analytics

--- header.php
+++ header.php
@@ -7,5 +7,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script async src="https://cdn.splitbee.io/sb.js"></script>
   </head>
   <body>
